d07e9642ae1e0ec90efe0e4a49b3066e969ec548
PaulaMarquezM <paulabemar2006@gmail.com>
2025-02-15 11:01:31 -0500

    Update PacienteDTO.java

--- src/main/java/ec/webmarket/restful/dto/v1/PacienteDTO.java
+++ src/main/java/ec/webmarket/restful/dto/v1/PacienteDTO.java
@@ -1,12 +1,12 @@
 package ec.webmarket.restful.dto.v1;
 
-import lombok.Data;
 import java.time.LocalDate;
+import lombok.Data;
 
 @Data
 public class PacienteDTO {
     private Long id;
-    private String numeroCedula;
+    private Long numeroCedula;
     private String nombre;
     private String apellido;
     private String telefono;
